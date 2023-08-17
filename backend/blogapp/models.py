6d916247a6ed744b704ac54f934adadd62d7b4cb
Rafsa Asman <aspharnamsat@gmail.com>
2023-08-17 14:24:55 +0300

    created classes and the models at models.py

--- backend/blogapp/models.py
+++ backend/blogapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+ blogTitle=models.CharField(max_length=(150))
+ blogPost=models.TextField()
+ author=models.CharField(max_length=(50))
+ date_published=models.DateTimeField(default=timezone.now)
