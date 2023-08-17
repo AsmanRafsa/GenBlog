180ff5203af878cd10602c874cd954edabc248e8
Rafsa Asman <aspharnamsat@gmail.com>
2023-08-17 14:52:02 +0300

    created a super user

--- backend/blogapp/models.py
+++ backend/blogapp/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
 class Blog(models.Model):
- blogTitle=models.CharField(max_length=(150))
- blogPost=models.TextField()
- author=models.CharField(max_length=(50))
- date_published=models.DateTimeField(default=timezone.now)
+    blogTitle=models.CharField(max_length=(150))
+    blogPost=models.TextField()
+    author=models.CharField(max_length=(50))
+    date_published=models.DateTimeField(default=timezone.now())
+    
+    def __str__(self):
+        return self.blogTitle 
+
