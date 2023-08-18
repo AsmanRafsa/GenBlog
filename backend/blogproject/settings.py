0e64b5bfdd06583cbf179fcc7a7ad92f8fde8190
Rafsa Asman <aspharnamsat@gmail.com>
2023-08-18 12:36:23 +0300

    created a database

--- backend/blogproject/settings.py
+++ backend/blogproject/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'blogproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'blog',
+        'PASSWORD':'Aspharnamsat@gma1l.c0m',
+        'USER':'postgres'
     }
 }
 
