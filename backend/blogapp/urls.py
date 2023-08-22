18135262427030ffd23db55a1c86ee6198d57626
Rafsa Asman <aspharnamsat@gmail.com>
2023-08-22 11:46:39 +0300

    created API

--- backend/blogapp/urls.py
+++ backend/blogapp/urls.py
@@ -4,8 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('blogs', views.BlogView.as_view(),name='blogs')
-]
+    path('blogs', views.BlogView.as_view(),name='blogs'),
+    path('blogs/<id>', views.SingleBlogView.as_view(),name='single-blog')
+    ]
+
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
     
--- backend/blogapp/views.py
+++ backend/blogapp/views.py
@@ -2,21 +2,57 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import Http404
 from .models import Blog
 from .serializers import BlogSerializer
 # Create your views here.
+#  All blogs
 class BlogView(APIView):
     def get(self,request,format=None):
+        # Getting from the data base
         blogs=Blog.objects.all()
+        # Converting to JSON
         serializer=BlogSerializer(blogs,many=True)
         return Response(serializer.data)
     def post(self,request,format=None):
-        
         serializers=BlogSerializer(data=request.data)
         if serializers.is_valid():
             serializers.save()
             return Response(serializers.data,status=status.HTTP_201_CREATED)
         return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    #Single blog
+class SingleBlogView(APIView):
+    def get_single_blog(self,id):
+        try:
+            return Blog.objects.get(id=id)
+        except Blog.DoesNotExist:
+            raise Http404
+        
+    # Getting a single blog
+    def get(self, request, id ):
+        blog = self.get_single_blog(id)
+        serializer =BlogSerializer(blog)
+        return Response(serializer.data)
+    # updating a single blog
+    def put(self,request,id):
+        blog=self.get_single_blog(id)
+        serializer=BlogSerializer(blog,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    #Deleting a single blog
+    def delete(self,request,id):
+        blog=self.get_single_blog(id)
+        blog.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+        
+               
+        
+        
+        
         
         
     
